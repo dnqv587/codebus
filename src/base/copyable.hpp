dd85fb58817cbb75b3c63deb8cf520a77344c724
dnqv587 <1336047549@qq.com>
2023-09-15 00:55:38 +0800

    可拷贝类

---
+++ src/base/copyable.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+//***********************************************
+// name      : copyable.hpp
+// brief     : 可拷贝类
+// attention : None
+// date      : 2023/9/15
+//***********************************************
+class copyable
+{
+public:
+    copyable()=default;
+    ~copyable()=default;
+    copyable& operator=(const copyable&)=default;
+    copyable(const copyable&)=default;
+};
