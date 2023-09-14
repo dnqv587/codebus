7e649767554c6e69695fd248403468728cc165e5
dnqv587 <1336047549@qq.com>
2023-09-15 00:57:48 +0800

    当前线程

--- src/thread/CurrentThread.h
+++ src/thread/CurrentThread.h
@@ -1,15 +1,20 @@
 #pragma once
-#include <pthread.h>
+#include <sys/types.h>
+#include <base/noncopyable.hpp>
 
-class CurrentThread
+class CurrentThread :noncopyable
 {
 public:
-	int32_t tid() const
-	{
-		return t_threadId;
-	}
-
-private:
+    /// @brief 线程号
+    /// @return
+    static pid_t tid();
+    /// @brief 线程名
+    /// @return
+    static const char* ThreadName();
+    /// @brief 是否为主线程
+    /// @return
+    static bool isMainThread();
+protected:
 	thread_local static const char* t_threadName;
-	thread_local static int t_threadId;
-}
+	thread_local static pid_t t_threadId;
+};
