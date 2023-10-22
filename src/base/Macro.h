696de147aa92a6efa103ad996279f9a2d3a1ca1f
dnqv587 <1336047549@qq.com>
2023-10-22 22:40:23 +0800

    apender clang thread attribute

--- src/base/Macro.h
+++ src/base/Macro.h
@@ -14,4 +14,88 @@
 #else
 #	define LIKELY(x)      (x)
 #	define UNLIKELY(x)    (x)
-#endif 
+#endif
+
+
+//clang的线程安全注解--需加入编译选项 -Wthread-safety
+// https://clang.llvm.org/docs/ThreadSafetyAnalysis.html
+
+#if defined(__clang__) && (!defined(SWIG))
+#define THREAD_ANNOTATION_ATTRIBUTE__(x)   __attribute__((x))
+#else
+#define THREAD_ANNOTATION_ATTRIBUTE__(x)   // no-op
+#endif
+
+//CAPABILITY 表明某个类对象可以当作 capability 使⽤，其中 x 的类型是 string，能够在错误信息当中指出对应的 capability 的名称
+#define CAPABILITY(x) \
+  THREAD_ANNOTATION_ATTRIBUTE__(capability(x))
+
+//SCOPED_CAPABILITY ⽤于修饰基于 RAII 实现的 capability
+#define SCOPED_CAPABILITY \
+  THREAD_ANNOTATION_ATTRIBUTE__(scoped_lockable)
+//end
+
+//GUARD_BY ⽤于修饰对象，表明该对象需要受到 capability 的保护
+#define GUARDED_BY(x) \
+  THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(x))
+
+//PT_GUARDED_BY⽤于修饰指针类型变量，在更改指针变量所指向的内容前需要加锁，否则发出警告
+#define PT_GUARDED_BY(x) \
+  THREAD_ANNOTATION_ATTRIBUTE__(pt_guarded_by(x))
+
+//ACQUIRED_BEFORE 和 ACQUIRED_AFTER 主要⽤于修饰 capability 的获取顺序，⽤于避免死锁
+#define ACQUIRED_BEFORE(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(acquired_before(__VA_ARGS__))
+
+#define ACQUIRED_AFTER(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(acquired_after(__VA_ARGS__))
+
+//REQUIRES 声明调⽤线程必须拥有对指定的 capability 具有独占访问权。可以指定多个 capabilities。函数/⽅法在访问资源时，必须先上锁，再调⽤函数，然后再解锁
+#define REQUIRES(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(requires_capability(__VA_ARGS__))
+
+//REQUIRES_SHARED 功能与 REQUIRES 相同，但是可以共享访问
+#define REQUIRES_SHARED(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(requires_shared_capability(__VA_ARGS__))
+
+//CQUIRE 表⽰⼀个函数/⽅法需要持有⼀个 capability，但并不释放这个 capability。调⽤者在调⽤被 ACQUIRE 修饰的函数/⽅法时，要确保没有持有任何 capability，进入函数时才会持有capability
+#define ACQUIRE(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(acquire_capability(__VA_ARGS__))
+
+//ACQUIRE_SHARED 与 ACQUIRE 的功能是类似的，但持有的是共享的 capability
+#define ACQUIRE_SHARED(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(acquire_shared_capability(__VA_ARGS__))
+
+//RELEASE 和 RELEASE_SHARED 与 ACQUIRE 和 ACQUIRE_SHARED 正相反，它们表⽰调⽤⽅在调⽤该函数/⽅法时需要先持有锁，⽽当函数执⾏结束后会释放锁
+#define RELEASE(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(release_capability(__VA_ARGS__))
+
+#define RELEASE_SHARED(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(release_shared_capability(__VA_ARGS__))
+
+#define TRY_ACQUIRE(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_capability(__VA_ARGS__))
+
+#define TRY_ACQUIRE_SHARED(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_shared_capability(__VA_ARGS__))
+
+//EXCLUDES ⽤于显式声明函数/⽅法不应该持有某个特定的 capability。由于 mutex 的实现通常是不可重⼊的，因此 EXCLUDES 通常被⽤来预防死锁
+#define EXCLUDES(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(locks_excluded(__VA_ARGS__))
+
+//ASSERT_* 表⽰在运⾏时检测调⽤线程是否持有 capability
+#define ASSERT_CAPABILITY(x) \
+  THREAD_ANNOTATION_ATTRIBUTE__(assert_capability(x))
+
+#define ASSERT_SHARED_CAPABILITY(x) \
+  THREAD_ANNOTATION_ATTRIBUTE__(assert_shared_capability(x))
+
+//RETURN_CAPABILITY 通常⽤于修饰那些被当作 capability getter 的函数，这些函数会返回 capability 的引⽤或指针
+#define RETURN_CAPABILITY(x) \
+  THREAD_ANNOTATION_ATTRIBUTE__(lock_returned(x))
+
+//NO_THREAD_SAFETY_ANALYSIS 表⽰关闭某个函数/⽅法的 TSA 检测，通常只⽤于两种情况：1，该函数/⽅法可以被做成⾮线程安全；2、函数/⽅法太过复杂
+#define NO_THREAD_SAFETY_ANALYSIS \
+  THREAD_ANNOTATION_ATTRIBUTE__(no_thread_safety_analysis)
+
+
