8f33a7711dfd49316e651805155382fe7ed4c60b
dainq45972 <1336047549@qq.com>
2024-01-06 14:30:12 +0800

    test

--- test/LoggerTest.cc
+++ test/LoggerTest.cc
@@ -1,6 +1,8 @@
 #include <log/Logger.h>
+#include <config/config.h>
 #include <unistd.h>
 #include <iostream>
+#include <exception/Exception.hpp>
 
 Logger logger("test");
 MutexLock mutex;
@@ -19,19 +21,79 @@ void func()
     }
 }
 
+
+void test( std::function<const char* ()> str)
+{
+    std::cout<<GET_CONSTEVAL(str)<<std::endl;
+    //const char* ss=StringUtil::BaseFileName(GET_CONSTEVAL(str));
+    //std::cout<<ss<<std::endl;
+    //str +=2;
+    //return GET_CONSTEVAL(str);
+}
+
+class Foo
+{
+public:
+    template<typename T>
+    constexpr explicit Foo(T file)
+    :name(StringUtil::BaseFileName(GET_CONSTEVAL(file)))
+    {
+		std::cout<<name<<std::endl;
+    }
+    friend std::ostream& operator<<(std::ostream& lhs , Foo foo)
+    {
+        lhs<<foo.name;
+        return lhs;
+    }
+
+    constexpr const char* getName() const
+    {
+        return this->name;
+    }
+private:
+    const char* name;
+};
+
+template<typename T>
+const char* print(T ff)
+{
+    //printf("%s", ff.getName());
+	const char* str=StringUtil::BaseFileName(GET_CONSTEVAL(ff));
+	//return str;
+
+	return str;
+}
+
 int main()
 {
-    _i=0;
+    //static_assert(StringUtil::BaseFileName(__FILE__));
 
+	/*
+    const char* str=print(CONSTEVAL(__FILE__));
+	std::cout<<str<<std::endl;
+
+
+    //constexpr const char* str=StringUtil::BaseFileName(__FILE__);
+   // const char* (*ptr)()=CONSTEVAL(__FILE__);
+    std::function<const char* ()> ptr=CONSTEVAL(__FILE__);
+    test(CONSTEVAL(__FILE__));
+    //std::cout<<ss<<std::endl;
+    auto val= CONSTEVAL(StringUtil::BaseFileName(__FILE__));
+    const char* sss= GET_CONSTEVAL(val);
+    std::cout<<sss<<std::endl;*/
+
+    //constexpr auto i=Logging(logger,StringUtil::BaseFileName(__FILE__),__LINE__,__FUNCTION__,LogLevel::ERROR);
+    _i=0;
+/*
     logger.setFormatter("%d{%Y-%m-%d %H:%M:%S}%T%t%T%N%T%F%T[%p]%T[%c]%T%f:%l%T%m%n");
     logger.setLevel(LogLevel::TRACE);
 
-    LogAppender::ptr SyncAppender=LogAppender::ptr(new FileLogAppender("SyncAppender",AppenderAction::SYNC,3,1024,4_MB));
+    LogAppender::ptr SyncAppender=LogAppender::ptr(new FileLogAppender("SyncAppender","log",AppenderAction::SYNC,3,1024,4_MB));
     //LogAppender::ptr AsyncAppender=LogAppender::ptr(new FileLogAppender("AsyncAppender",AppenderAction::ASYNC,1,1024,10*FileSize::MB));
-    //LogAppender::ptr logout=LogAppender::ptr(new StdoutLogAppender());
+    LogAppender::ptr logout=LogAppender::ptr(new StdoutLogAppender());
     logger.addAppender(SyncAppender);
     //logger.addAppender(AsyncAppender);
-    //logger.addAppender(logout);
+    logger.addAppender(logout);*/
 
     //Thread thread(func,"testFunc");
     //thread.run();
@@ -42,6 +104,15 @@ int main()
 //    LOG_FATAL(logger)<<"这里是颜色测试";
 //    LOG_TRACE(logger)<<"这里是颜色测试";
 //    LOG_WARN(logger)<<"这里是颜色测试";
+    ConfigManager::getInstance()->Load("test","test.toml");
+	LoggerManager::getInstance()->LoadConfig("test");
+	try {
+		LOG_THROW(Exception::LoggerError("这是测试 log"));
+	}
+	catch (...)
+	{
+		std::cout<<"test"<<std::endl;
+	}
 
     while(_i<65535)
     {
@@ -49,8 +120,8 @@ int main()
 		//++_i;
 		//MutexLockGuard lock(mutex);
         //sleep(1);
-        LOG_ERROR(logger) <<  "这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 " << "---" << ++_i;
-        //LOG_FMT_ERROR(logger,"%s%s%d","这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 ","---",++_i);
+        LOG_INFO(LOG_ROOT) <<  "这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 " << "---" << ++_i;
+        LOG_FMT_ERROR(GET_LOGGER("test"),"%s%s%d","这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 ","---",++_i);
 
     }
     //thread.join();
--- test/TomlTest.cc
+++ test/TomlTest.cc
@@ -4,9 +4,9 @@
 
 static constexpr const std::string_view str=R"(
         str = "hello world"
-        time = 2019-10-12 14:20:50.52+12:00
+        time = 1979-05-27T00:32:00.9999999991Z
 
-        numbers = [ 1, 2, 3, "four", 5.0 ]
+        numbers = [ 1, 2, 3, [ "tomato", "onion", "mushroom", "lettuce" ], 5.0 ]
         vegetables = [ "tomato", "onion", "mushroom", "lettuce" ]
         minerals = [ "quartz", "iron", "copper", "diamond" ]
 
@@ -20,12 +20,14 @@ int main()
 {
     toml::table tb=toml::parse(str);
 
-    //std::cout<<tb.at_path("str").as_integer()->get()<<std::endl;
+    std::cout<<tb.at_path("str").as_string()->get()<<std::endl;
     std::cout<<*tb.at_path("time").as_date_time()<<std::endl;
+    //std::cout<<tb.at_path("animals.cats").as_table()<<std::endl;
 
-    tb["animals"]["cats"].as_array()->for_each([](auto&& val){
+    std::cout<<tb.at_path("animals").as_table()<<std::endl;
+    tb.at_path("numbers").as_array()->for_each([](auto&& val,int i){
 
-        //std::cout<<val<<std::endl;
+        std::cout<<val<<i<<std::endl;
     });
     //std::cout<<toml::json_formatter(tb)<<std::endl;
     return 0;
