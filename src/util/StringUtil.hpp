0bbf55b7563cc9d6f0f5148f3b19dcab297dd2a2
dainq45972 <1336047549@qq.com>
2024-01-13 10:56:25 +0800

    增加字符串转文件大小类型

--- src/util/StringUtil.hpp
+++ src/util/StringUtil.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <base/Type.hpp>
 
 namespace StringUtil
 {
@@ -15,6 +16,44 @@ constexpr const char* BaseFileName(const char (&filename)[N])
         }
     }
     return filename;
+}
+
+
+constexpr off64_t StrToFileSize(const char* str)
+{
+    off64_t size=0;
+    while(str)
+    {
+        if(*str>='0' && *str<='9')
+        {
+            size = size*10+(*str-'0');
+        }
+        else
+        {
+            if(*(str+1)!='B')
+            {
+                return size;
+            }
+            switch(*str)
+            {
+                case 'M':
+                    return operator""_MB(size);
+                    break;
+                case 'G':
+                    return operator""_GB(size);
+                    break;
+                case 'T':
+                    return operator""_TB(size);
+                    break;
+                default:
+                    return size;
+            }
+        }
+        ++str;
+
+    }
+
+    return size;
 }
 
 
