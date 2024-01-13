93b269d0d9cfe5ca63195bb889d0aa2f2654c137
dainq45972 <1336047549@qq.com>
2024-01-13 10:57:06 +0800

    test

--- test/LoggerTest.cc
+++ test/LoggerTest.cc
@@ -7,82 +7,21 @@
 Logger logger("test");
 MutexLock mutex;
 
-//std::atomic<int> _i;
-int _i;
+std::atomic<int> _i;
+//int _i;
 
 void func()
 {
     while(_i<65535)
     {
-		std::cout<<++_i<<std::endl;
-		//MutexLockGuard lock(mutex);
-        //sleep(1);
-		//LOG_WARN(logger) << "aqewqcasacsad德农请我哦对你抢我电脑青豌豆你请我对哦IQ我你起期待你起哦对去hbafiosuc haiuowdbnciqwducioq4829120640186的缺点去外地请问带我去749861 8q7g4 r789q0wef4q86---" <<"--:"<<++_i;
+		LOG_WARN(logger) << "aqewqcasacsad德农请我哦对你抢我电脑青豌豆你请我对哦IQ我你起期待你起哦对去hbafiosuc haiuowdbnciqwducioq4829120640186的缺点去外地请问带我去749861 8q7g4 r789q0wef4q86---" <<"--:"<<++_i;
     }
 }
 
-
-void test( std::function<const char* ()> str)
-{
-    std::cout<<GET_CONSTEVAL(str)<<std::endl;
-    //const char* ss=StringUtil::BaseFileName(GET_CONSTEVAL(str));
-    //std::cout<<ss<<std::endl;
-    //str +=2;
-    //return GET_CONSTEVAL(str);
-}
-
-class Foo
-{
-public:
-    template<typename T>
-    constexpr explicit Foo(T file)
-    :name(StringUtil::BaseFileName(GET_CONSTEVAL(file)))
-    {
-		std::cout<<name<<std::endl;
-    }
-    friend std::ostream& operator<<(std::ostream& lhs , Foo foo)
-    {
-        lhs<<foo.name;
-        return lhs;
-    }
-
-    constexpr const char* getName() const
-    {
-        return this->name;
-    }
-private:
-    const char* name;
-};
-
-template<typename T>
-const char* print(T ff)
-{
-    //printf("%s", ff.getName());
-	const char* str=StringUtil::BaseFileName(GET_CONSTEVAL(ff));
-	//return str;
-
-	return str;
-}
 
 int main()
 {
-    //static_assert(StringUtil::BaseFileName(__FILE__));
 
-	/*
-    const char* str=print(CONSTEVAL(__FILE__));
-	std::cout<<str<<std::endl;
-
-
-    //constexpr const char* str=StringUtil::BaseFileName(__FILE__);
-   // const char* (*ptr)()=CONSTEVAL(__FILE__);
-    std::function<const char* ()> ptr=CONSTEVAL(__FILE__);
-    test(CONSTEVAL(__FILE__));
-    //std::cout<<ss<<std::endl;
-    auto val= CONSTEVAL(StringUtil::BaseFileName(__FILE__));
-    const char* sss= GET_CONSTEVAL(val);
-    std::cout<<sss<<std::endl;*/
-
-    //constexpr auto i=Logging(logger,StringUtil::BaseFileName(__FILE__),__LINE__,__FUNCTION__,LogLevel::ERROR);
     _i=0;
 /*
     logger.setFormatter("%d{%Y-%m-%d %H:%M:%S}%T%t%T%N%T%F%T[%p]%T[%c]%T%f:%l%T%m%n");
@@ -95,33 +34,25 @@ int main()
     //logger.addAppender(AsyncAppender);
     logger.addAppender(logout);*/
 
-    //Thread thread(func,"testFunc");
-    //thread.run();
-
-//    LOG_ERROR(logger)<<"这里是颜色测试";
-//    LOG_INFO(logger)<<"这里是颜色测试";
-//    LOG_DEBUG(logger)<<"这里是颜色测试";
-//    LOG_FATAL(logger)<<"这里是颜色测试";
-//    LOG_TRACE(logger)<<"这里是颜色测试";
-//    LOG_WARN(logger)<<"这里是颜色测试";
     ConfigManager::getInstance()->Load("test","test.toml");
 	LoggerManager::getInstance()->LoadConfig("test");
-	try {
-		LOG_THROW(Exception::LoggerError("这是测试 log"));
-	}
-	catch (...)
-	{
-		std::cout<<"test"<<std::endl;
-	}
 
-    while(_i<65535)
+    LOG_ERROR(LOG_ROOT)<<"这里是颜色测试";
+    LOG_INFO(LOG_ROOT)<<"这里是颜色测试";
+    LOG_DEBUG(LOG_ROOT)<<"这里是颜色测试";
+    LOG_FATAL(LOG_ROOT)<<"这里是颜色测试";
+    LOG_TRACE(LOG_ROOT)<<"这里是颜色测试";
+    LOG_WARN(LOG_ROOT)<<"这里是颜色测试";
+    //Thread thread(func,"testFunc");
+    //thread.run();
+    while(_i<655350)
     {
 		//logger.trace("测试日志输出");
 		//++_i;
 		//MutexLockGuard lock(mutex);
         //sleep(1);
         LOG_INFO(LOG_ROOT) <<  "这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 " << "---" << ++_i;
-        LOG_FMT_ERROR(GET_LOGGER("test"),"%s%s%d","这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 ","---",++_i);
+        LOG_FMT_ERROR(GET_LOGGER("test"),"%s%s%d","这里是测试logger的性能 1234567890 这里是测试logger的性能 abcdefghijklmnopqrstuvwxyz 这里是测试logger的性能 ABCDEFGHIJKLMNOPQRSTUVWXYZ  中文输入性能测试 ","---",_i.load());
 
     }
     //thread.join();
